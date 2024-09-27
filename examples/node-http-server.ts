5fd9d8bbaf9ea0b48a1bad5dde1ea95d4faa6d9f
trapcodeio <trapcode.io@gmail.com>
2024-09-27 17:10:26 +0100

    Save

--- examples/node-http-server.ts
+++ examples/node-http-server.ts
@@ -1,16 +1,44 @@
 import { __dirname, init } from "@xpresser/framework";
 import NodeHttpServerProvider from "@xpresser/server-module/servers/NodeHttpServerProvider.js";
 
-// Initialize Xpresser
-const $ = await init({
-    env: "development",
-    name: "Basic Xpresser App",
-    paths: { base: __dirname(import.meta.url) }
-});
+await WithXpresserHandler();
 
-const { router } = await NodeHttpServerProvider.use($);
+async function WithXpresserHandler() {
+    // Initialize Xpresser
+    const $ = await init({
+        env: "development",
+        name: "Express Server with Xpresser Request Handler",
+        paths: { base: __dirname(import.meta.url) }
+    });
 
-router.get("/", () => "Hello World!");
+    const { router } = await NodeHttpServerProvider.use($, {
+        // Set server module as default module
+        defaultModule: true
+    });
 
-// Start Xpresser
-$.start().catch($.console.logErrorAndExit);
+    router.get("/", () => "Hello World!");
+
+    // Start Xpresser
+    $.start().then(WithNativeHandler).catch($.console.logErrorAndExit);
+}
+
+async function WithNativeHandler() {
+    const $ = await init({
+        env: "development",
+        name: "Express Server with Native Request Handler",
+        paths: { base: __dirname(import.meta.url) },
+        server: {
+            port: 2001
+        }
+    });
+
+    const { nativeRouter } = await NodeHttpServerProvider.use($, {
+        defaultModule: true,
+        requestHandler: "native"
+    });
+
+    nativeRouter.get("/", (_req, res) => res.end("Hello From Express Router"));
+
+    // Start Xpresser
+    $.start().catch($.console.logErrorAndExit);
+}
